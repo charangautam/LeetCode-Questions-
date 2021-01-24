2651eca207e2d4d0a6a74b26274b1fe45177b25c
Charan Gautam <64233303+charangautam@users.noreply.github.com>
2021-01-23 21:37:25 -0700

    Add files via upload

---
+++ BinaryTreeLevelOrderTraversal II.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    let result = [];
+    
+    if(root === null) return result;
+    
+    let queue = [];
+    queue.push(root)
+    
+    while(queue.length > 0){
+        let l = queue.length;
+        let temp = [];
+        
+        for(let i=0; i<l; i++){
+            let current = queue.shift();
+            temp.push(current.val);
+            
+            if(current.left !== null) queue.push(current.left);
+            if(current.right !== null) queue.push(current.right);
+        }
+        result.unshift(temp);
+    }
+    
+    return result;
+};
