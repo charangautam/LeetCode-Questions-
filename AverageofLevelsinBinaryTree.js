91ef0c318f414e81a2cae24b1af2f5db563f65b3
Charan Gautam <64233303+charangautam@users.noreply.github.com>
2021-02-03 21:56:00 -0700

    Add files via upload

---
+++ AverageofLevelsinBinaryTree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+    let result = [];
+    
+    if(root === null) return result;
+    
+    let queue = [];
+    queue.push(root);
+    
+    while(queue.length > 0){
+        let l = queue.length ;
+        let temp = 0;
+        
+        for(let i=0; i<l; i++){
+            let current = queue.shift();
+    
+            temp += current.val
+            
+            if(current.left !== null) queue.push(current.left);
+            if(current.right !== null) queue.push(current.right);
+        }
+        temp = temp/l;
+        result.push(temp);
+    }
+    return result;
+};
