ab17651d5bc05fa7080bb7b07cd988f08f63938f
Charan Gautam <64233303+charangautam@users.noreply.github.com>
2021-02-28 21:32:25 -0700

    Add files via upload

---
+++ IntersectionofTwoArrays.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let output = [];
+    let map = {};
+    for (let i=0; i<nums1.length; i++) {
+        map[nums1[i]] = true;
+    }
+    for (let i=0; i<nums2.length; i++) {
+        if (map[nums2[i]]) {
+            output.push(nums2[i]);
+            delete map[nums2[i]];
+        }
+    }
+    return output;
+};
