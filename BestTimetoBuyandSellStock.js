fe20368ee2df85b456ad33c43982f7f9eeeb5664
Charan Gautam <64233303+charangautam@users.noreply.github.com>
2021-03-11 22:28:13 -0700

    Add files via upload

---
+++ BestTimetoBuyandSellStock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    if(prices.length < 0) return 0;
+    let max = 0;
+    let min = prices[0];
+    
+    for(let i=0; i<prices.length; i++){
+        min = Math.min(prices[i], min);
+        max = Math.max(prices[i] - min, max);
+    }
+    return max;
+};
