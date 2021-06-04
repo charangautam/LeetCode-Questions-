54cade1719c558765bf6b69dfc5e987708829adc
Charan Gautam <64233303+charangautam@users.noreply.github.com>
2021-06-03 22:08:00 -0600

    Add files via upload

---
+++ ValidAnagram.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+ var isAnagram = function(s, t) {
+    let map = {}
+    
+    if(s.length !== t.length) return false
+    
+    for(let i=0; i<s.length; i++){
+        map[s[i]] = map[s[i]] + 1 || 1
+    }
+    
+    for(let i=0; i<t.length; i++){
+        if(map[t[i]]) {
+            map[t[i]]--
+            if(map[t[i]] < 0) return false
+        } else {
+            return false
+        }
+    }
+    
+    return true
+};
