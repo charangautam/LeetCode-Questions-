ca3b53b48fa65f7f06053abfa3f5ea5f82f5eb88
Charan Gautam <64233303+charangautam@users.noreply.github.com>
2022-03-21 23:01:27 -0600

    1. Two Sum

--- TwoSum.js
+++ TwoSum.js
@@ -3,16 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
+var twoSum = function(nums, target) {
     let map = {}
-
-    for (let i = 0; i < nums.length; i++) {
+    
+    for(let i=0; i<nums.length; i++) {
         let x = target - nums[i]
-
-        if (x in map) {
+        
+        if(x in map) {
             return [map[x], i]
         }
-
+        
         map[nums[i]] = i
     }
 };
