2c6556660ff2650de02975d41eaae54c7b4a8961
Charan Gautam <64233303+charangautam@users.noreply.github.com>
2022-03-21 22:58:14 -0600

    Add files via upload

--- TwoSum.js
+++ TwoSum.js
@@ -3,16 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    const map = {};
-    const l = nums.length;
-    
-    for (let i = 0; i<l; i++){
-        let x = target - nums[i];
-        if(map[x] >= 0){
+var twoSum = function (nums, target) {
+    let map = {}
+
+    for (let i = 0; i < nums.length; i++) {
+        let x = target - nums[i]
+
+        if (x in map) {
             return [map[x], i]
         }
-        
-        map[nums[i]] = i;
+
+        map[nums[i]] = i
     }
 };
