435bdb5daa047bd22ff94df6c7ecda19be2ab3a2
Charan Gautam <64233303+charangautam@users.noreply.github.com>
2022-03-22 18:01:22 -0600

    1929. Concatenation of Array

--- ConcatenationofArray.js
+++ ConcatenationofArray.js
@@ -2,6 +2,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// built in JS function 
  var getConcatenation = function(nums) {
     return nums.concat(nums)
+};
+
+// spread operator 
+var getConcatenation = function(nums) {
+    return [...nums, ...nums]
+};
+
+// brute
+var getConcatenation = function(nums) {
+    let l = nums.length;
+    
+    for(let i=0; i<l; i++){
+        nums.push(nums[i]);
+    }
+    
+    return nums;
 };
